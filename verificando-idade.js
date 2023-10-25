3cabe65e8ae1785888f6d6f64dde1ff1a7019d6c
RudsonE <rudsonemanoel12345@gmail.com>
2023-10-24 23:11:48 -0300

    Adicionado exemplos de codigo

---
+++ verificando-idade.js
@@ -0,0 +1,9 @@
+const nome = "Rudson";
+const idade = 24;
+const bebidaMaior = "cerveja";
+const bebidaMenor = "suco";
+
+const pedido = `${nome} diz: "por favor, quero beber ${idade >= 18 ? bebidaMaior : bebidaMenor}"`;
+
+
+console.log(pedido);
