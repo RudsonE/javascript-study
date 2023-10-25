948240e4107b9c4c62f042ca45b5e025fcd58bac
RudsonE <rudsonemanoel12345@gmail.com>
2023-10-25 20:58:08 -0300

    arrow funtions e expressoes

---
+++ expressoes-funcao.js
@@ -0,0 +1,26 @@
+//function minhaFuncao(param){
+// //bloco de código
+//}
+
+//minhaFuncao("param")
+
+//expressao de função
+
+//const soma = function(num1, num2){
+ //   return num1 + num2
+//}
+//console.log(soma(1,1))
+
+//diferença principal: HOISTING
+// funções e var são "listadas" no topo
+
+console.log(apresentar());
+
+function apresentar(){
+    return "Olá";
+}
+
+console.log(soma(1, 1))
+const soma = function(num1, num2){
+    return num1 + num2
+}
