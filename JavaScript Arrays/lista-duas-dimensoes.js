c7fbbe7cc9358b503d4a09e83996e62265b85519
RudsonE <rudsonemanoel12345@gmail.com>
2023-10-26 20:36:15 -0300

    Pastas reorganizadas e adicionados novos itens

---
+++ JavaScript Arrays/lista-duas-dimensoes.js
@@ -0,0 +1,9 @@
+const alunos = ["João", "Juliana", "Caio", "Ana"];
+const medias = [10, 8, 7.5, 9];
+
+const lista2d = [alunos, medias];
+
+console.log(lista2d);
+
+console.log(`A aluna da posição 1 da lista de alunos é ${lista2d[0][1]}
+e sua média é ${lista2d[1][1]}`);
