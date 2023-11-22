f73c679a0b2252be89461c742132d837ff62efa8
RudsonE <rudsonemanoel12345@gmail.com>
2023-11-22 00:40:41 -0300

    Estudos de promisses

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,3 +1,19 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.blue('olá mundo!'));
+
+function trataErro(erro){
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+
+function pegaArquivo(caminhoDoArquivo){
+    const enconding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArquivo, enconding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+}
+
+pegaArquivo('./arquivos/texto.md')
