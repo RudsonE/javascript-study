4b52d6b0992d27ba636ac2088b38bdd9899efe1a
RudsonE <rudsonemanoel12345@gmail.com>
2023-11-22 10:58:14 -0300

    Teste com async/await

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,20 +1,32 @@
 import fs from 'fs';
 import chalk from 'chalk';
-import fs from 'fs';
 
 
-function trataErro(erro){
+function trataErro(erro) {
     console.log(erro);
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
 
-function pegaArquivo(caminhoDoArquivo){
+//async/await
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const enconding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        console.log(chalk.green(texto))
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+//usando then
+/*function pegaArquivo(caminhoDoArquivo) {
     const enconding = 'utf-8';
     fs.promises
         .readFile(caminhoDoArquivo, enconding)
         .then((texto) => console.log(chalk.green(texto)))
         .catch(trataErro)
 }
-
+*/
 pegaArquivo('./arquivos/texto.md')
+pegaArquivo('./arquivos/')
